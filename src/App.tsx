f4beeadd4351270ab14897447c258f78eb6582ab
david <davidabu91@gmail.com>
2021-11-11 12:28:21 +0200

    Move state up to App level, start Focus screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, {SetStateAction, useState} from 'react';
+import { BrowserRouter, Route, Routes, NavLink  } from 'react-router-dom';
+import FocusScreen from './screens/FocusScreen';
 import ListScreen from './screens/ListScreen';
+import { Task } from './types';
 
 
 function App() {
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const tasksProps = {tasks, setTasks};
+
   return (
     <BrowserRouter>
-    <Routes >
-      <Route  path='/' element={<ListScreen/>}/>
-      <Route path='/focus'></Route>
-    </Routes>
+      <nav>
+        <NavLink to="/" >List</NavLink>
+        {' '}-{' '}
+        <NavLink to="/focus">Focus</NavLink>
+
+      </nav>
+      <br />
+      <Routes >
+        <Route path='/' element={<ListScreen {...tasksProps} />} />
+        <Route path='/focus' element={<FocusScreen {...tasksProps}/>}/>
+      </Routes>
     </BrowserRouter>
   );
 }
